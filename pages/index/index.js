95bb466d4bfc7dc69acb018d9cede871865f7b0f
13396406725 <2213958088@qq.com>
2021-02-13 07:59:51 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+// index.js
+// 获取应用实例
+const app = getApp();
+
+Page({
+  data: {
+    background: ["demo-text-1", "demo-text-2", "demo-text-3"],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+  },
+  onLoad() {},
+  goto(type) {
+    switch (type) {
+      case 1:
+        wx.navigateTo({
+          url: "/pages/active/active",
+        });
+        break;
+
+      default:
+        break;
+    }
+  },
+});
