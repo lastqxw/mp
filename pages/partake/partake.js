17d1d3d242bfbc7ce5c48a50300a1de37e30a0f6
lx <1018767774@qq.com>
2021-02-19 15:17:25 +0800

    feat:完成我的参与页面

---
+++ pages/partake/partake.js
@@ -0,0 +1,71 @@
+// pages/partake/partake.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    parTake:[
+      {
+        id:'1',
+        title:'关于公司发展方向的讨论',
+        loc:'第三会议室',
+        des:'带着纸笔前往会议室',
+        date:'',
+        time:'16:00',
+        state:'1'
+      },
+      {
+        id:'2',
+        title:'关于公司发展方向的讨论',
+        loc:'第三会议室',
+        des:'带着纸笔前往会议室',
+        date:'',
+        time:'16:00',
+        state:'2'
+      },
+      {
+        id:'3',
+        title:'关于公司发展方向的讨论',
+        loc:'第三会议室',
+        des:'带着纸笔前往会议室',
+        date:'02-16',
+        time:'16:00',
+        state:'3'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+  // 参加活动
+  intoActive(){
+    wx.navigateTo({
+      url: '../active/active'
+    });
+  },
+  // 活动详情
+  toDetails(e){
+    wx.navigateTo({
+      url: '../activity/activity?id='+e.currentTarget.dataset.id+'&state='+e.currentTarget.dataset.state
+    });
+  }
+})
